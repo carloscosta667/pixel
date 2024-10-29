1b7fa9038b27d20f21b855fd3438a4db0d686605
carlos costa <green_carloscosta@hotmail.com>
2024-10-29 21:56:24 +0000

    Create api.php

---
+++ config/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Message Response
+    |--------------------------------------------------------------------------
+    |
+    | All possible messages from the endpoints
+    |
+    */
+
+    'service' => [
+        'car' => [
+            'get' => [
+                'success' => 'Get car service successful',
+                'error'   => 'Get car service not successful',
+            ],
+            'post' => [
+                'success' => 'Car service successful created',
+                'error'   => 'Car service not successful created'
+            ],
+            'put' => [
+                'success' => 'Car service successful edited',
+                'error'   => 'Car service not successful edited',
+            ],
+            'delete' => [
+                'success' => 'Car service  successful deleted',
+                'error'   => 'Car service not successful deleted'
+            ]
+        ]
+    ],
+
+    'something_wrong' => 'Something went wrong!'
+
+];

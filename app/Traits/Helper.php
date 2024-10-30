d8f5c047645be2bd38c582db4fd56531774a17c9
carlos costa <green_carloscosta@hotmail.com>
2024-10-30 00:10:26 +0000

    Update Helper.php
    create a generic return response for unknown errors

--- app/Traits/Helper.php
+++ app/Traits/Helper.php
@@ -2,6 +2,10 @@
 
 namespace app\Traits;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
 trait Helper
 {
     /**
@@ -9,13 +13,35 @@ trait Helper
      *
      * @param $data
      * @param $message
-     * @return array
+     * @param $status_code
+     * @return JsonResponse
      */
-    public function responseFormat($data, $message): array
+    public function responseFormat($data, $message, $status_code): JsonResponse
     {
-        return [
+        return response()->json(
+            [
             'data' => $data,
             'message' => $message
-        ];
+            ],
+            $status_code
+        );
     }
+
+    /**
+     * The format response for unknown error
+     *
+     * @param $exception
+     * @return JsonResponse
+     */
+    public function responseFormatUnknown($exception): JsonResponse
+    {
+        //log any critical issue to be easier to debug
+        Log::critical($exception->getMessage());
+        return response()->json(
+            [
+                'message' => config('api.something_wrong')
+            ]
+            , Response::HTTP_BAD_GATEWAY);
+    }
+
 }

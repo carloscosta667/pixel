64d8f51781ffc8fec9fa81f1039e54787eb2230b
carlos costa <green_carloscosta@hotmail.com>
2024-10-30 09:04:42 +0000

    Fix lower case to upper case

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,13 @@
 
 namespace Database\Factories;
 
-use app\Models\User;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {

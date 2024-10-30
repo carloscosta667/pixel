2819109f98a5aee7bc8c606e1e8f0c8642321672
carlos costa <green_carloscosta@hotmail.com>
2024-10-30 00:20:13 +0000

    Create UserController.php
    Create login function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use App\Models\User as UserModel;
+use app\Traits\helper;
+use http\Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    use Helper;
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        try {
+
+            $data = [];
+            $credentials = $request->only('email', 'password');
+
+            //check validation
+            $validatedData = Validator::make(
+                $request->all(),
+                UserModel::rulesLogin()
+            );
+
+            if($validatedData->fails()){
+                return $this->responseFormat($data, $validatedData->errors()
+                    , Response::HTTP_BAD_REQUEST);
+            }
+
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+
+                if(is_null($user->email_verified_at)){
+                    return $this->responseFormat($data, config('api.account_not_activated')
+                        , Response::HTTP_BAD_REQUEST);
+                }
+
+                //get the bearer token to response
+                $bearer_token = time().'-'.Str::random(60);
+                $user->api_token = $bearer_token;
+
+                $user->save();
+
+                return $this->responseFormat($user, config('api.login_success')
+                    , Response::HTTP_OK);
+            }
+
+            return  $this->responseFormat($data, config('api.login_invalid')
+                , Response::HTTP_BAD_REQUEST);
+
+        } catch ( Exception $e) {
+
+            return $this->responseFormatUnknown($e);
+
+        }
+
+    }
+}
